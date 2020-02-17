8590c2bc35ad697c410f1f26cac4d3c388a3d0f8
KokoTa <584847514@qq.com>
2020-02-17 18:00:00 +0800

    feat: 路径字典

---
+++ components/model/judger.js
@@ -0,0 +1,19 @@
+/**
+ * 路径字典判断类，SKU 核心控制类
+ */
+
+class Judger {
+  fenceGroup
+
+  constructor(fenceGroup) {
+    this.fenceGroup = fenceGroup
+  }
+
+  init() {
+
+  }
+}
+
+export {
+  Judger
+}
